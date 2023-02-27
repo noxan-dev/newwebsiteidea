9d018fb38fcf987bfc5597b74bac9d16656619d9
Chaim Malek <chaimmalek@gmail.com>
2023-02-27 12:17:16 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<script>
+  let name = 'Chaim'
+</script>
+
+<h1>{name.toUpperCase()}</h1>
