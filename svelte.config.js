75e984a1665b884fff39e14a7aafde46b62dff9c
Noxan <59417659+noxan-dev@users.noreply.github.com>
2023-08-20 17:40:30 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-
-const dev = process.argv.includes('dev');
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    paths: {
-      base: dev ? '/newwebsiteidea' : process.env.BASE_PATH,
-    }
-  }
+ // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+ // for more information about preprocessors
+ preprocess: vitePreprocess(),
+
+ kit: {
+  adapter: adapter(),
+ }
 };
+
+export default config;
